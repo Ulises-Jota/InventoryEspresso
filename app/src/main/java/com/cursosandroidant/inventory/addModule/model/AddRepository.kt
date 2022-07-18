8e95cb0c18661c1a51346acc1b1305ea5853559b
Javier Tapia <javiertapia.sin.numeros@gmail.com>
2022-07-17 21:30:25 -0300

    InventoryEspresso project

---
+++ app/src/main/java/com/cursosandroidant/inventory/addModule/model/AddRepository.kt
@@ -0,0 +1,12 @@
+package com.cursosandroidant.inventory.addModule.model
+
+import com.cursosandroidant.inventory.common.MyDataBase
+import com.cursosandroidant.inventory.entities.Product
+
+class AddRepository {
+    private val myDataBase by lazy { MyDataBase.getInstance() }
+
+    fun addProduct(product: Product, callback: (isSuccess: Boolean) -> Unit){
+        callback(myDataBase.add(product))
+    }
+}
---
+++ app/src/main/java/com/cursosandroidant/inventory/mainModule/view/adapters/OnClickListener.kt
@@ -0,0 +1,8 @@
+package com.cursosandroidant.inventory.mainModule.view.adapters
+
+import com.cursosandroidant.inventory.entities.Product
+
+interface OnClickListener {
+    fun onClick(product: Product)
+    fun onLongClick(product: Product)
+}
